3f40fd7523ba6857939beb252ae73b27a8bab5d6
Kate Rossinskaya <kateros@siber.com>
2023-09-05 19:36:41 +0200

    add: app base

---
+++ app/src/main/java/com/example/splitthebill/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.splitthebill
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.splitthebill.databinding.MainActivityLayoutBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: MainActivityLayoutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = MainActivityLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
