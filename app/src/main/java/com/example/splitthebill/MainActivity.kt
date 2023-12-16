30206042ec6e2eb54f2389cbf07a64b748e9f954
Kate Rossinskaya <rossinskaya98@gmail.com>
2023-12-16 23:39:41 +0100

    KR: add dark theme

--- app/src/main/java/com/example/splitthebill/MainActivity.kt
+++ app/src/main/java/com/example/splitthebill/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.splitthebill
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import com.example.splitthebill.databinding.FMainActivityBinding
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FMainActivityBinding.inflate(layoutInflater)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.DarkAppTheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
         setContentView(binding.root)
     }
 
