94304c3181b9730a43415f7f8b1fc3df99f7e0ad
Kate Rossinskaya <rossinskaya98@gmail.com>
2024-03-12 12:45:11 +0300

    KR: update build gradle

--- app/src/main/java/com/example/splitthebill/MainActivity.kt
+++ app/src/main/java/com/example/splitthebill/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.splitthebill
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import com.example.splitthebill.databinding.FMainActivityBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,26 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = FMainActivityBinding.inflate(layoutInflater)
-        val currentNightMode = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
-            setTheme(R.style.DarkAppTheme)
-        } else {
-            setTheme(R.style.AppTheme)
-        }
         setContentView(binding.root)
-    }
-
-    fun addFragment(fragment: Fragment){
-        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(binding.fragmentContainerView.id, fragment, fragment.toString())
-        fragmentTransaction.addToBackStack(fragment.toString())
-        fragmentTransaction.commit()
-    }
-
-    fun replaceFragment(fragment: Fragment){
-        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.replace(binding.fragmentContainerView.id, fragment, fragment.toString())
-        //fragmentTransaction.addToBackStack(fragment.toString())
-        fragmentTransaction.commit()
     }
 }
